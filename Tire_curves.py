6286719f84a619ab8c2f35f3e2ed7d0be8d659ce
rahul3142 <59677603+rahul3142@users.noreply.github.com>
2020-09-21 16:57:54 -0400

    Add files via upload

---
+++ Tire_curves.py
@@ -0,0 +1,38 @@
+import pandas as pa
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+to_be_compared_list = ('datafile1.dat', 'datafile2.dat')
+# add the data files of interest in the folder and update the link(s) in the list
+
+
+def slip_angles(link):
+    run = pa.read_csv(f'{link}', sep='\t', header=1)
+    n_column = []
+
+    for i in range(run.columns.size):
+        temp = run.columns[i] + ' (' + run.iloc[0, i] + ')'
+        n_column.append(temp)
+
+    run.columns = n_column
+    reformatted = run.drop([0])
+    reformatted = reformatted[:7300]
+    reformatted = reformatted.astype(float)
+
+    sns.set()
+    plt.plot()
+    plt.plot(reformatted['SA (deg)'], reformatted['FY (lb)'])
+
+
+for file in to_be_compared_list:
+    slip_angles(f'{file}')
+
+plt.ylabel('Lateral Load (N)')
+plt.xlabel('Slip Angles (deg)')
+plt.title('Random Tire data Curves')
+plt.axis([-13, 13, -650, 650])
+
+plt.savefig('tire_data.png', dpi=400)
+plt.show()
+
+quit()
